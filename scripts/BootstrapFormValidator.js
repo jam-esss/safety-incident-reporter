f9c174d900818b742c822aa18e9696da1fa1b7c1
James <76698733+jam-esss@users.noreply.github.com>
2026-04-07 17:01:38 +0100

    Made BootstrapFormValidator.js universal for all form validation.

--- scripts/BootstrapFormValidator.js
+++ scripts/BootstrapFormValidator.js
@@ -1,17 +1,20 @@
-const form = document.getElementById('contactForm');
+const forms = document.querySelectorAll('form');
 const successMessage = document.getElementById('successMessage');
 
-form.addEventListener('submit', function (event) {
-    // Prevent default submission and add 'was-validated' class.
-    event.preventDefault();
-    form.classList.add('was-validated');
 
-    // If form is not valid, stop here and let Bootstrap show errors.
-    if (!form.checkValidity()) {
-        return;
-    }
+forms.forEach((form) => {
+    form.addEventListener('submit', function (event) {
+        // Prevent default submission and add 'was-validated' class.
+        event.preventDefault();
+        form.classList.add('was-validated');
 
-    // If form is valid, hide the form and display success message.
-    form.style.display = 'none';
-    successMessage.style.display = 'block';
-});
+        // If form is not valid, stop here and let Bootstrap show errors.
+        if (!form.checkValidity()) {
+            return;
+        }
+
+        // If form is valid, hide the form and display success message.
+        form.style.display = 'none';
+        successMessage.style.display = 'block';
+    });
+})
